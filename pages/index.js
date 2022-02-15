52e585c5a11541b4d26fe839dce16b1dfa95c10a
michee <micheebakanda@gmail.com>
2022-02-15 13:16:24 +0200

    completed price-section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Nav from "../components/Nav";
 import Banner from "../components/Banner";
 import WhatWeOffer from "../components/WhatWeOffer";
+import Price from "../components/Price";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
     <Nav />
     <Banner />
     <WhatWeOffer />
+    <Price />
     </>
   )
 }
